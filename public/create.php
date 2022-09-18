2f25c2be8a91caed1193fb4fb9560a4235478a42
double-happ1ness <double.happiness@outlook.com>
2022-09-18 12:03:28 +0100

    added new create.php

---
+++ public/create.php
@@ -0,0 +1,58 @@
+<?php 
+
+include "../config/settings.php";
+require '../config/dependencies.php';
+
+  if (isset($_POST['submit'])) {
+    $assetTag = $_POST['assetTag'];
+    $assignedTo = $_POST['assignedTo'];
+    $dateBought = $_POST['dateBought'];
+    $dateDecommisioned = $_POST['dateDecommisioned'];
+    $deviceType = $_POST['deviceType'];
+    $operatingSystem = $_POST['operatingSystem'];
+
+    $sql = "INSERT INTO `devices`(`assetTag`, `assignedTo`, `dateBought`, `dateDecommisioned`, `deviceType`, `operatingSystem`) 
+    VALUES ('$assetTag','$assignedTo','$dateBought','$dateDecommisioned','$deviceType','$operatingSystem')";
+
+    $result = $pdo->query($sql);
+
+    if ($result == TRUE) {
+      echo "New record created successfully.";
+    }else{
+      echo "Error:". $sql . "<br>". $pdo->error;
+    } 
+
+    $pdo->close(); 
+
+  }
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+<body>
+
+<h2>Add new device:</h2>
+
+<form action="" method="POST">
+
+  <fieldset>
+    <legend>Device:</legend>
+    assetTag:<br>
+    <input type="text" name="assetTag"><br>
+    assignedTo:<br>
+    <input type="text" name="assignedTo"><br>
+    dateBought:<br>
+    <input type="date" name="dateBought"><br>
+    dateDecommisioned:<br>
+    <input type="date" name="dateDecommisioned"><br>
+    deviceType:<br>
+    <input type="text" name="deviceType"><br>
+    operatingSystem:<br>
+    <input type="text" name="operatingSystem"><br><br>
+    <input type="submit" name="submit" value="submit">
+  </fieldset>
+</form>
+</body>
+</html>
